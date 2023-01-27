d04f03b49ed1b50a8ce51e14cd287010f0b6b815
istiyakmi9 <istiyaq.mi9@gmail.com>
2023-01-27 21:28:34 +0530

    added new db ip address

--- src/main/java/com/bottrack/TrackerApplication.java
+++ src/main/java/com/bottrack/TrackerApplication.java
@@ -2,12 +2,28 @@ package com.bottrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "PUT", "POST", "DELETE", "HEAD")
+						.allowCredentials(false);
+			}
+		};
 	}
 }
