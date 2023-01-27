12c61d0e51ce9e8b74a0947a69422784e7a89659
Istiyak <md.istiyak@protractor.com>
2023-01-27 15:21:28 +0530

    jwt authentication

--- src/main/java/com/bottrack/service/UserService.java
+++ src/main/java/com/bottrack/service/UserService.java
@@ -1,47 +1,48 @@
 package com.bottrack.service;
 
-import com.bottrack.repository.UserRepository;
 import com.bottrack.model.User;
+import com.bottrack.repositoryinterfaces.IUserRepository;
 import com.bottrack.serviceinterfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements IUserService {
 
     @Autowired
-    UserRepository userRepository;
+    IUserRepository iUserRepository;
 
     public String addUserService(User user) throws Exception {
-        var result = this.userRepository.addUserRepository(user);
+        var result = this.iUserRepository.save(user);
+        return "New user has been added";
+    }
+
+    public ResponseEntity<Object> updateUserService(User user, long userId) {
+        Optional<User> result = this.iUserRepository.findById(userId);
+        if (result.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        user.setUserId(userId);
+        iUserRepository.save(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    public List<User> getAllUserService(){
+        var result = this.iUserRepository.findAll();
         return result;
     }
 
-    public String updateUserService(User user, long userId) throws IOException {
-        var result = "";
-        if (userId > 0){
-            result = this.userRepository.updateUserRepository(user, userId);
-            if (result == null || result == "")
-            throw new IOException("Unable to update");
-        }
-        return result;
-    }
-
-    public ArrayList<User> getAllUserService(){
-        var result = this.userRepository.getAllUserRepository();
-        return result;
-    }
-
-    public ArrayList<User> getByUserIdService(long userId) {
-        var result = this.userRepository.getByUserIdRepository(userId);
+    public Optional<User> getByUserIdService(long userId) {
+        var result = this.iUserRepository.findById(userId);
         return result;
     }
 
     public String deleteByUserIdService(long userId) {
-        var result = this.userRepository.deleteByUserIdRepository(userId);
-        return result;
+        this.iUserRepository.deleteById(userId);
+        return "User data has been deleted";
     }
 }
