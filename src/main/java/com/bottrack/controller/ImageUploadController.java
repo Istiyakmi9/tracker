ed4006b2056d946dc44b05917d0ef76cc411739a
Istiyak <md.istiyak@protractor.com>
2023-03-22 15:59:04 +0530

    conflict resolved

---
+++ src/main/java/com/bottrack/controller/ImageUploadController.java
@@ -0,0 +1,26 @@
+package com.bottrack.controller;
+
+import com.bottrack.model.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+@RestController
+@RequestMapping("/ImageUploader")
+public class ImageUploadController {
+
+    @PostMapping("/uploadImage")
+    public ResponseEntity<ApiResponse> uploadImage(@RequestParam("file")MultipartFile file){
+        String fileName = file.getOriginalFilename();
+        try {
+            file.transferTo(new File("D:\\" + fileName));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.ok(ApiResponse.Ok("Working successfully.."));
+    }
+
+}
