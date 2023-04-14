2d0df158b1494ff1ec971d446c64e2653dea4c6a
Marghub <marghub12@gmail.com>
2023-04-14 11:12:48 +0530

    Vehicle insert update with image

--- src/main/java/com/bottrack/controller/VehicleDetailController.java
+++ src/main/java/com/bottrack/controller/VehicleDetailController.java
@@ -1,9 +1,7 @@
 package com.bottrack.controller;
 
 import com.bottrack.model.ApiResponse;
-import com.bottrack.model.User;
 import com.bottrack.model.VehicleDetail;
-import com.bottrack.repositorymodel.ResponseModal;
 import com.bottrack.service.VehicleDetailService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,17 +22,48 @@ public class VehicleDetailController extends BaseController {
     ObjectMapper objectMapper;
 
     @PostMapping("/addVehicleDetail")
-    public ResponseEntity<ApiResponse> addVehicleDetail(@RequestBody VehicleDetail vehicleDetail){
-        var result = this.vehicleDetailService.addVehicleDetailService(vehicleDetail);
+    public ResponseEntity<ApiResponse> addVehicleDetail(@RequestParam("vehicleDetail") String vehicleDetail,
+                                                        @RequestParam("file") MultipartFile file) throws Exception {
+        VehicleDetail vehicle = objectMapper.readValue(vehicleDetail, VehicleDetail.class);
+        var result = this.vehicleDetailService.addVehicleDetailService(vehicle, file);
         return ResponseEntity.ok(ApiResponse.Ok(result));
     }
 
+    @PutMapping("/updateVehicle/{vehicleId}")
+    public ResponseEntity<ApiResponse> updateVehicleDetail(@RequestParam("vehicleDetail") String vehicle,
+                                                           @RequestParam("file") MultipartFile file,
+                                                           @PathVariable("vehicleId") long vehicleId) throws Exception {
+
+        VehicleDetail vehicleDetail = objectMapper.readValue(vehicle, VehicleDetail.class);
+        var result = this.vehicleDetailService.updateVehicleDetailService(vehicleDetail, file, vehicleId);
+        return ResponseEntity.ok(ApiResponse.Ok(result));
+    }
+
+    @GetMapping("/getVehicleByEmail/{email}")
+    public ResponseEntity<ApiResponse> getVehicleByEmail(@PathVariable("email") String email) throws Exception {
+        var result = this.vehicleDetailService.getVehicleByEmailService(email);
+        return ResponseEntity.ok(ApiResponse.Ok(result));
+    }
+
+    @GetMapping("/getVehicleByMobile/{mobile}")
+    public ResponseEntity<ApiResponse> getVehicleByMobile(@PathVariable("mobile") String mobile) throws Exception {
+        var result = this.vehicleDetailService.getVehicleByMobileService(mobile);
+        return ResponseEntity.ok(ApiResponse.Ok(result));
+    }
+
+    @GetMapping("/getVehicleByUserId/{userId}")
+    public ResponseEntity<ApiResponse> getVehicleByUserId(@PathVariable("userId") Long userId) throws Exception {
+        var result = this.vehicleDetailService.getVehicleByUserIdService(userId);
+        return ResponseEntity.ok(ApiResponse.Ok(result));
+    }
+
+//    Old Code
+
     @PutMapping("/updateVehicleDetailByVehicleNo/{vehicleNo}")
-    public ResponseEntity<ApiResponse> updateVehicleDetailByVehicleNo(@RequestBody VehicleDetail vehicleDetail, @PathVariable("vehicleNo") long vehicleNo) throws IOException {
+    public ResponseEntity<ApiResponse> updateVehicleDetailByVehicleNo(@RequestBody VehicleDetail vehicleDetail, @PathVariable("vehicleNo") String vehicleNo) throws IOException {
         var result = this.vehicleDetailService.updateVehicleDetailByVehicleNoService(vehicleDetail, vehicleNo);
         return ResponseEntity.ok(ApiResponse.Ok(result));
     }
-
     @GetMapping("/getAllVehicleDetail")
     public ResponseEntity<ApiResponse> getAllVehicleDetail(){
         var result = this.vehicleDetailService.getAllVehicleDetailService();
@@ -63,4 +92,6 @@ public class VehicleDetailController extends BaseController {
         var result = this.vehicleDetailService.createOrUpdateVehicleDetailService(vehicleDetail, vehicleNo, userId, file);
         return ResponseEntity.ok(ApiResponse.Ok(result));
     }
+
+
 }
